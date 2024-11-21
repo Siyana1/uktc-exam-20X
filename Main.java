fd4055d1505c22db0be4dc6d4994f58f8ec36657
Siyana1 <siydymar@gmail.com>
2024-11-21 13:48:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
 public class Main {
 
-    public static void main(String[] args) {
-        Mouse mouse = new Mouse;
+    public static void main(String[] args) 
+        Mouse mouse = new Mouse();
         mouse.squeak();
     }
 
+
     static class Mouse {
-        squeak() {
-            System.println("Squeak");
+    
+        void squeak() {
+            System.out.println("Squeak");
         }
     }
 }
